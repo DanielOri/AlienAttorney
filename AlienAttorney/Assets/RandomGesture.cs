d2c4512b359fd360ca75da4ba5fe05f7fc2a0638
Daniel Ribeiro <danielribeiro.new@gmail.com>
2015-03-11 18:23:43 -0400

    Hand is casting a raycast. Raycasting script attached to the index finger of the hand prefab. to do: make stuff happen when you point at something.

---
+++ AlienAttorney/Assets/RandomGesture.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class RandomGesture : MonoBehaviour {
+
+	public GameObject light1, light2;
+
+	Leap.Controller controller;
+
+	// Use this for initialization
+	void Start () {
+	
+		controller = new Controller();
+		controller.EnableGesture(Gesture.GestureType.TYPECIRCLE, true);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+		Frame frame = controller.Frame();
+		foreach (Gesture gesture in frame.Gestures())
+		{
+			switch(gesture.Type)
+			{
+			case(Gesture.GestureType.TYPECIRCLE):
+			
+				Debug.Log("Circle gesture recognized.");
+				break;
+
+				default:
+				Debug.Log("Bad gesture type");
+				break;
+
+			}
+		
+			}
+		}
+
+	}
+
---
+++ AlienAttorney/Assets/Raycasting.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycasting : MonoBehaviour {
+
+	public RaycastHit hit;
+	Ray ray;
+	public bool onObj = false;
+	public GameObject light;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		ray = new Ray( transform.position, transform.forward);//Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		Debug.DrawRay(ray.origin, ray.direction, Color.green);
+
+		if (Physics.Raycast(ray, out hit)){
+
+			Debug.Log ("hit");
+
+			if (hit.collider.tag == "Cube"){
+				Debug.Log("hit cube");
+				onObj = true;
+				//hit.collider.gameObject.GetComponent<Light>().enabled =true;
+				
+				
+			} else {
+				onObj = false;
+				//hit.collider.gameObject.GetComponent<Light>().enabled =false;
+
+			}
+
+
+		}
+
+		if (onObj){
+			GameObject.Find("Light").GetComponent<Light>().enabled = true;
+
+
+		} else {
+			GameObject.Find("Light").GetComponent<Light>().enabled = false;
+		}
+
+
+
+	}
+
+
+}
