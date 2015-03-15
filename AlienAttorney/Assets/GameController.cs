d0f9adf5c906f47acc4b5be7447305bd1dda171e
Daniel Ribeiro <danielribeiro.new@gmail.com>
2015-03-15 19:51:44 -0400

    Hands have their mesh renderers disabled.

--- AlienAttorney/Assets/GameController.cs
+++ AlienAttorney/Assets/GameController.cs
@@ -3,15 +3,41 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject button;
+	public ButtonPress button;
+	public Light opt1, opt2, opt3;
+
+
+
 
 	// Use this for initialization
 	void Start () {
+		//button = GetComponent<ButtonPress>();
+
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (button.isPressed && opt1.enabled){
+
+			Debug.Log ("option 1 selected!");
+			opt1.color = Color.red;
+
+		}
+
+		if (button.isPressed && opt2.enabled){
+			
+			Debug.Log ("option 2 selected!");
+			opt2.color = Color.blue;
+			
+		}
+
+		if (button.isPressed && opt3.enabled){
+			
+			Debug.Log ("option 3 selected!");
+			opt3.color = Color.green;
+			
+		}
 	}
 }
